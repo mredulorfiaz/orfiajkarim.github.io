085bb208b0f42ee2b2c216b15332d785af82a029
Orfiaj Karim <orfiajkarim@gmail.com>
2020-12-03 15:42:05 +0600

    fixes

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -22,5 +22,29 @@ export const portfolioData = [
     demourl: "https://jahidulsaeid.github.io/react-pics",
     githuburl: "",
     description: ""
+  },
+  {
+    id: 4,
+    name: "Katling Clothing - Women Clothing/Fashion Wear/Maternity Wear",
+    img: "./img/pic5.png",
+    demourl: "https://katling.com.au",
+    githuburl: "",
+    description: ""
+  },
+  {
+    id: 5,
+    name: "Pet Shopping Website",
+    img: "./img/pic5.png",
+    demourl: "https://www.australianpetdoors.com.au/",
+    githuburl: "",
+    description: ""
+  },
+  {
+    id: 5,
+    name: "Cafe & Catering Website",
+    img: "./img/pic6.png",
+    demourl: "https://whycookalameda.com/",
+    githuburl: "",
+    description: ""
   }
 ];
